f87ad575abdaffccfb15ad5ddac7d723c10d9ad5
Yogarajalakshmi S <yogarajalakshmis@gmail.com>
2025-02-23 18:50:15 -0500

    connection fix

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://myuser:test@localhost:5432/orders"
+DATABASE_URL = "postgresql://myuser:test@db:5432/orders"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
