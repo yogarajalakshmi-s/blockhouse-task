e2ce425ead3fd4bea4cd76751d08219405c7fcfa
Yogarajalakshmi S <yogarajalakshmis@gmail.com>
2025-02-23 19:32:48 -0500

    connectio error fix

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://myuser:test@18.117.12.227:5432/orders"
+DATABASE_URL = "postgresql://myuser:test@18.188.112.220:5432/orders"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
